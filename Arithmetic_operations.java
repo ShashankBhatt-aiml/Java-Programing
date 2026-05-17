f6bcd1488cba62d995c6832b3029171a0ec9daf9
ShashankBhatt-aiml <shashankbhatt8171@gmail.com>
2026-05-17 11:45:22 +0530

    Initial commit

---
+++ Arithmetic_operations.java
@@ -0,0 +1,16 @@
+public class Arithmetic_operations {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 75;
+
+        int sum = a + b;
+        int subs = b - a;
+        int mul = a * b;
+        int div = b / a;
+
+        System.out.println("Sum is:-" + sum);
+        System.out.println("subs is:-" + subs);
+        System.out.println("mul is:-" + mul);
+        System.out.println("div is:-" + div);
+    }
+}
---
+++ Var_Dtypes.java
@@ -0,0 +1,14 @@
+class Var_Dtypes {
+  public static void main(String[] args){
+    // Variable and data types.
+    int age = 19;
+    double marks = 92.75;
+    String name = "Shashank";
+    boolean state = true;
+
+    System.out.println("Name is :- " + name);
+    System.out.println("Age is :- " + age);
+    System.out.println("Total Percentage is:- " + marks);
+    System.out.println(state);
+  }
+}
